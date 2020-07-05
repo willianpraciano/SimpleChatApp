4d93eff1bab3652ca38d011c1ea59f0079c69790
Willian Praciano <willian.s.praciano@gmail.com>
2020-07-05 17:45:41 -0300

    base of chat screen built

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View, 
+  Text
+} from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = (props) => {
+
+  const user = {
+    _id: 1,
+    name: 'Maria',
+    avatar: 'url'
+  };
+
+  const messages = [{
+    _id: 1,
+    text: 'Ola Pessoal',
+    createdAt: new Date(),
+    user:{
+      _id: 1,
+      name: 'João',
+      avater: 'url' 
+    }
+  }];
+
+  function onSendeMessage(messages){
+    console.log(messages);
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <GiftedChat user={user} messages={messages} onSend={onSendeMessage}/>
   );
 }
 
